ad847b4241b8bb72330e9aa5c69975b17eac2686
Pablo Donato <pablo.donato@mailoo.org>
2017-03-13 15:06:07 +0100

    Add predicter agent class

---
+++ predicter.py
@@ -0,0 +1,22 @@
+class Predict:
+    def __init__(self, eta, batch_size...):
+        self.eta = eta
+        self.image = None
+
+    def train(image):
+        pass
+
+    def accuracy(self, image, label):
+        pred = self.predict(image)
+        return cost
+
+    def act(self, observation, reward, done, info):
+        if self.image is None:
+            self.image = info['cursor']
+            return
+
+        print(self.accuracy(self.image, info['cursor']))
+        return (10, (0,1))
+
+    def predict(image):
+        return pred
