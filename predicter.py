008d844cbf453a63d46763d8f6c8e1c37f212b64
Pablo Donato <pablo.donato@mailoo.org>
2017-03-24 12:59:18 +0100

    Random action

--- predicter.py
+++ predicter.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 class Predicter:
     def __init__(self, learning_rate,  nbp_input, time_training):
-        self.X = tf.placeholder("float32", [None, nbp_input])
+        self.X = tf.placeholder("float32", [None, nbp_input + 1])
         self.Y = tf.placeholder("float32", [None, nbp_input])
         n_l1 = 256 
         n_l2 = 128
         weights = {
-            'encoder_l1': tf.Variable(tf.random_normal([nbp_input, n_l1])),
+            'encoder_l1': tf.Variable(tf.random_normal([nbp_input + 1, n_l1])),
             'encoder_l2': tf.Variable(tf.random_normal([n_l1, n_l2])),
             'decoder_l1': tf.Variable(tf.random_normal([n_l2, n_l1])),
             'decoder_l2': tf.Variable(tf.random_normal([n_l1, nbp_input])),
@@ -37,8 +38,9 @@ class Predicter:
 
         self.image = None
 
-    def train(self, image, next_image):
-        cost, _ = self.sess.run([self.cost, self.optimizer], feed_dict={self.X: image, self.Y: next_image})
+    def train(self, image, direction, next_image):
+        input_ = np.concatenate((image, [[direction]]), axis=1)
+        cost, _ = self.sess.run([self.cost, self.optimizer], feed_dict={self.X: input_, self.Y: next_image})
         return cost
 
     def predict(self, image):
@@ -53,16 +55,17 @@ class Predicter:
             self.image = None
             return
 
+        action = (10, random.randint(0, 3))
         next_image = info['cursor'].reshape(1,-1).astype(np.float32) / 255
 
         if self.image is None:
             self.image = next_image
-            return (10, (0, 1))
+            return action
 
-        cost = self.train(self.image, next_image)
+        cost = self.train(self.image, action[1], next_image)
         print(1 - cost)
 
         self.image = next_image
 
         # Return action (don't try to predict digit, move in the same direction)
-        return (10, (0, 1))
+        return action
